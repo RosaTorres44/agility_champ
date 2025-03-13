3117f2a1007c4933af2f2ff0810939a4b05f32cc
Rosa María Torres Guerra <rosamtorres4492@gmail.com>
2025-03-13 15:10:36 -0500

    usuario de conexion a bd

--- data/db.ts
+++ data/db.ts
@@ -2,10 +2,10 @@ import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,      
-  port: process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : 3306,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10, // Número recomendado de conexiones
   queueLimit: 0,
